0929ba4c9eab0a3bbe1dc4972b93cb8697777551
Anton_Stepanenko <anton_stepanenko72009@ukr.net>
2025-09-09 16:34:07 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import { initMongoConnection } from './db/initMongoConnection.js';
-import { setupServer } from './server.js';
+import { initMongoConnection } from "./db/initMongoConnection.js";
+import express from "express";
+import cors from "cors";
+import { contactsRouter } from "./routes/contacts.js";
+import { notFoundHandler, errorHandler } from "./middlewares/errorHandler.js";
 
 const bootstrap = async () => {
-  await initMongoConnection(); 
-  setupServer();
+  await initMongoConnection();
+
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+
+  app.use("/contacts", contactsRouter);
+
+  app.use(notFoundHandler);
+
+  app.use(errorHandler);
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server is running on port ${PORT}`);
+  });
 };
 
 bootstrap();
--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,20 @@
+import createHttpError from "http-errors";
+
+export const notFoundHandler = (req, res, next) => {
+  next(createHttpError(404, "Resource not found"));
+};
+
 export const errorHandler = (err, req, res, next) => {
-  res.status(err.status || 500).json({
-    status: err.status || 500,
+  if (err.status) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.message,
+    });
+    return;
+  }
+
+  res.status(500).json({
+    status: 500,
     message: "Something went wrong",
     data: err.message,
   });

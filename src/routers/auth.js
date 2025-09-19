5f3ff107aa8a83173e595914bdd1a0da4f1a4fdd
Anton_Stepanenko <anton_stepanenko72009@ukr.net>
2025-09-19 12:55:59 +0300

    first commit

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { registerController, loginController, refreshController, logoutController, getProfileController, sendResetEmailController, resetPasswordController } from "../controllers/auth.js";
 import { validateBody } from "../middlewares/validateBody.js";
-import { createAuthSchema, loginSchema, resetEmailSchema, resetPasswordSchema } from "../validation/auth.js";
+import { createAuthSchema, loginSchema, sendResetSchema, resetPwdSchema } from "../validation/auth.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = Router();
@@ -11,7 +11,7 @@ router.post("/login", validateBody(loginSchema), loginController);
 router.post("/refresh", refreshController);
 router.post("/logout", logoutController);
 router.get("/me", authMiddleware, getProfileController);
-router.post("/send-reset-email", validateBody(resetEmailSchema), sendResetEmailController);
-router.post("/reset-pwd", validateBody(resetPasswordSchema), resetPasswordController);
+router.post("/send-reset-email", validateBody(sendResetSchema), sendResetEmailController);
+router.post("/reset-pwd", validateBody(resetPwdSchema), resetPasswordController);
 
 export default router;

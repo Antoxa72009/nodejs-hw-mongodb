d1a6f978eb748569879f263bb6dc2aa5dd7a6d5f
Anton_Stepanenko <anton_stepanenko72009@ukr.net>
2025-09-20 23:45:34 +0300

    first commit

--- src/services/session.js
+++ src/services/session.js
@@ -1,4 +1,4 @@
-import Session from "../models/session.js";
+import { Session } from "../models/session.js";
 
 export const deleteUserSessionsForUser = async (userId) => {
   await Session.deleteMany({ userId });

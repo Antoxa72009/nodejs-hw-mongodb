4a85b90e53bb32f313f0433ce8a2184d5eee9dd3
Anton_Stepanenko <anton_stepanenko72009@ukr.net>
2025-09-27 23:55:54 +0300

    first commit

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import pino from "pino-http";
 import contactsRouter from "./routers/contacts.js";
 import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
+import { swaggerDocs } from "./middlewares/swaggerDocs.js";
 
 export const setupServer = () => {
   const app = express();
@@ -13,12 +14,15 @@ export const setupServer = () => {
   app.use(pino());
   app.use("/contacts", contactsRouter);
 
+  app.use("/api-docs", swaggerDocs());
+
   app.use(notFoundHandler);
   app.use(errorHandler);
 
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`🚀 Server is running on port ${PORT}`);
+    console.log(`📄 Swagger docs: http://localhost:${PORT}/api-docs`);
   });
 
   return app;
